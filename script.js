1a679e204e7e9ab2173de8ac85ef5a9be59347c0
saynotrish <anuran.nyleco24@gmail.com>
2023-05-03 15:56:00 +0800

    complete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleMenu = document.querySelector(".toggle__menu");
-const headNav = document.querySelector(".header__nav ul");
+const headNav = document.querySelector(".header__nav2");
+const header = document.querySelector(".header");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headNav.classList.toggle("open");
+  header.classList.toggle("open");
 });
